587e4a0092d8f934b927dd2749042e9dc4a2adbc
andykimpe1 <andykimpe@gmail.com>
2025-11-24 17:51:36 +0100

    Update use-standalone-widevine.sh

--- use-standalone-widevine.sh
+++ use-standalone-widevine.sh
@@ -7,12 +7,18 @@ if [ -z "$CHROMIUM_DIR" ]; then
 fi
 
 # Get Widevine archive
-./fetch-latest-widevine.sh -o ./widevine.zip
-# Expected directory structure:
-#	  widevine.zip
-#	  ├── LICENSE.txt
-#	  ├── manifest.json
-#	  └── libwidevinecdm.so
+#./fetch-latest-widevine.sh -o ./widevine.zip
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -O google-chrome-stable_current_amd64.deb
+ar xv google-chrome-stable_current_amd64.deb
+rm -f google-chrome-stable_current_amd64.deb
+rm -f debian-binary
+rm -f control.tar.xz
+xz -d data.tar.xz
+tar -xvf data.tar
+rm -f data.tar.xz data.tar
+rm -rf usr etc
+mv opt/google/chrome/WidevineCdm/* ./
+rm -rf opt
 
 # Extract Widevine and recreate this directory structure under the Chromium directory:
 #	  /usr/lib/chromium/WidevineCdm
@@ -21,13 +27,16 @@ fi
 #	  └── _platform_specific
 #		  └── linux_x64
 #				└── libwidevinecdm.so
-
+sudo rm -rf "$CHROMIUM_DIR/WidevineCdm/"
 sudo mkdir -p "$CHROMIUM_DIR/WidevineCdm/_platform_specific/linux_x64"
-unzip -p widevine.zip LICENSE.txt | sudo dd status=none of="${CHROMIUM_DIR}/WidevineCdm/LICENSE"
-unzip -p widevine.zip manifest.json | sudo dd status=none of="${CHROMIUM_DIR}/WidevineCdm/manifest.json"
-unzip -p widevine.zip libwidevinecdm.so | sudo dd status=none of="${CHROMIUM_DIR}/WidevineCdm/_platform_specific/linux_x64/libwidevinecdm.so"
+sudo cp LICENSE "${CHROMIUM_DIR}/WidevineCdm/"
+sudo cp manifest.json "${CHROMIUM_DIR}/WidevineCdm/"
+sudo _platform_specific/linux_x64/libwidevinecdm.so "$CHROMIUM_DIR/WidevineCdm/_platform_specific/linux_x64/"
+#unzip -p widevine.zip LICENSE.txt | sudo dd status=none of="${CHROMIUM_DIR}/WidevineCdm/LICENSE"
+#unzip -p widevine.zip manifest.json | sudo dd status=none of="${CHROMIUM_DIR}/WidevineCdm/manifest.json"
+#unzip -p widevine.zip libwidevinecdm.so | sudo dd status=none of="${CHROMIUM_DIR}/WidevineCdm/_platform_specific/linux_x64/libwidevinecdm.so"
 find "$CHROMIUM_DIR/WidevineCdm" -type d -exec sudo chmod 0755 '{}' \;
 find "$CHROMIUM_DIR/WidevineCdm" -type f -exec sudo chmod 0644 '{}' \;
 
 # clean up
-rm ./widevine.zip
+#rm ./widevine.zip
